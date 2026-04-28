78ada0d39153a5fd2e2966c8f851a09085e80379
Sakshi Ghanwat <141915486+SakshiGhanwat@users.noreply.github.com>
2026-04-28 10:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+let firstName = document.getElementById("fname").value.trim();
+            let lastName = document.getElementById("lname").value.trim();
 
+            alert(firstName + " " + lastName);
 }
